bbe4e37d1995fe17a4b8c3476e0b09c3e6c7a12f
Fabio Vige <fabiovige@gmail.com>
2023-12-31 08:36:12 -0300

    structs

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+type endereco struct {
+	rua    string
+	numero int
+	cidade string
+}
+
 func main() {
 	fmt.Println("Hello, World!")
+
+	endereco := endereco{
+		"Rua dos Bobos",
+		0,
+		"São Paulo",
+	}
+
+	fmt.Println(endereco)
+	endereco.numero = 449
+	fmt.Println(endereco)
 }
